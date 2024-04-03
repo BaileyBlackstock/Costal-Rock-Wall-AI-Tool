d2927c24d95d09835bd2cf80fbb72d55cb2b2ad0
Bailey Blackstock <140233889+BaileyBlackstock@users.noreply.github.com>
2024-04-04 00:08:43 +1000

    Filled in code for identify_reference_item function

--- AiImageAnalysis.py
+++ AiImageAnalysis.py
@@ -51,13 +51,40 @@ def identify_rock(image_path):
 
 def identify_reference_item(image_path):
     """
-    Dummy function to simulate processing an image.
-    :param image_path: Path to the image file.
-    :return: A string indicating result.
-    """
-    # Implement code here. For now, it's a placeholder.
-    return "Result of operation"
+    image_path: Path to the image file.
+    returns: An array containing an x of y pos of a pixel in the hard hat.
 
+    This function searches the given image to locate the position of the hard hat in the image.
+    It works by searching the centre of the image for the pixel closest to orange and returning
+    that pixels location.
+    
+    Prerequisites: the hard hat is in the centre of the image, the hard hat is orange, the hard
+    hat is the most orange thing in the centre of the image
+    """
+
+    orange = [37, 130, 255]  # colour to compare each pixel to (should be colour of hard hat)
+
+    # shrink the image to 480 pixels long to reduce computation time
+    sizeCap = 480
+    scale = sizeCap / len(RocksLarge[0])
+    Rocks = cv.resize(RocksLarge, (0, 0), fx=scale, fy=scale)
+    
+    best = 255  # diff between the pixel colour closest to orange
+    loc = []  # location of the pixel closest to orange
+    
+    # search only the middle of the image
+    thirdHeight = round(len(Rocks) / 3)
+    thirdLength = round(len(Rocks[0]) / 3)
+    
+    for x in range(thirdHeight, thirdHeight*2):
+        for y in range(thirdLength, thirdLength * 2):
+            val = Rocks[x][y]
+            diff = (abs(val[0] - orange[0]) + abs(val[1] - orange[1]) + abs(val[2] - orange[2])) / 3
+            if diff < best:
+                best = diff
+                loc = [round(x / scale), round(y / scale)]
+    
+    return loc  # An array containing a x and y positions of a pixel in the hard hat.
 
 def main():
     directory_path = "AI Image Analysis Data"  # Update this path
